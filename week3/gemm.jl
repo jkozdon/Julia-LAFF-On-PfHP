b7e3f791faf28da01484436862da3c2e9dbba965
Jeremy Edward Kozdon <jekozdon@nps.edu>
2019-12-27 09:57:58 -0800

    Cleanup a bit more

--- week3/gemm.jl
+++ week3/gemm.jl
@@ -1,56 +1,58 @@
 using SIMD
 
-function mygemm!(C, A, B, ::Val{MB}, ::Val{NB}) where {NB, MB}
+function mygemm!(C, A, B, ::Val{MR}, ::Val{NR}) where {NR, MR}
   (m, n) = size(C)
   (~, k) = size(A)
-  @assert mod(m, MB) == 0
-  @assert mod(n, NB) == 0
-  @inbounds for J = 1:NB:n
-    for I = 1:MB:m
+  @assert mod(m, MR) == 0
+  @assert mod(n, NR) == 0
+  @inbounds for J = 1:NR:n
+    for I = 1:MR:m
       @views k_mygemm(k,
-                      pointer(C, I + (J-1) * m),
-                      pointer(A, I),
-                      B,
-                      m,
-                      m,
-                      J, Val(MB), Val(NB))
+                      pointer(C, I + (J-1) * m), m,
+                      pointer(A, I), m,
+                      pointer(B, 1 + (J-1) * k), k,
+                      Val(MR), Val(NR))
     end
   end
 end
 
-function k_mygemm(k, C, A, B, ldc, lda, j, ::Val{MB}, ::Val{NB}) where {MB, NB}
+function k_mygemm(k,
+                  C, ldc,
+                  A, lda,
+                  B, ldb,
+                  ::Val{MR}, ::Val{NR}) where {MR, NR}
   T = eltype(A)
-  NB > 4 && error("NB too big")
+  NR > 4 && error("NR too big")
 
-  NB>0 && (c1 = vload(Vec{MB, T}, C + 0ldc*sizeof(T)))
-  NB>1 && (c2 = vload(Vec{MB, T}, C + 1ldc*sizeof(T)))
-  NB>2 && (c3 = vload(Vec{MB, T}, C + 2ldc*sizeof(T)))
-  NB>3 && (c4 = vload(Vec{MB, T}, C + 3ldc*sizeof(T)))
+  NR>0 && (c1 = vload(Vec{MR, T}, C + 0ldc*sizeof(T)))
+  NR>1 && (c2 = vload(Vec{MR, T}, C + 1ldc*sizeof(T)))
+  NR>2 && (c3 = vload(Vec{MR, T}, C + 2ldc*sizeof(T)))
+  NR>3 && (c4 = vload(Vec{MR, T}, C + 3ldc*sizeof(T)))
 
   @inbounds for p = 1:k
-    αv = vload(Vec{MB, T}, A + (p-1)*lda*sizeof(T))
+    αv = vload(Vec{MR, T}, A + (p-1)*lda*sizeof(T))
 
-    β = B[p, j + 0]
+    β = unsafe_load(B + (p-1 + 0ldb)*sizeof(T))
     c1 = muladd(β, αv, c1)
 
-    if NB > 1
-      β = B[p, j + 1]
+    if NR > 1
+      β = unsafe_load(B + (p-1 + 1ldb)*sizeof(T))
       c2 = muladd(β, αv, c2)
     end
 
-    if NB > 2
-      β = B[p, j + 2]
+    if NR > 2
+      β = unsafe_load(B + (p-1 + 2ldb)*sizeof(T))
       c3 = muladd(β, αv, c3)
     end
 
-    if NB > 3
-      β = B[p, j + 3]
+    if NR > 3
+      β = unsafe_load(B + (p-1 + 3ldb)*sizeof(T))
       c4 = muladd(β, αv, c4)
     end
   end
-  NB>0 && vstore(c1, C + 0ldc*sizeof(T))
-  NB>1 && vstore(c2, C + 1ldc*sizeof(T))
-  NB>2 && vstore(c3, C + 2ldc*sizeof(T))
-  NB>3 && vstore(c4, C + 3ldc*sizeof(T))
+  NR>0 && vstore(c1, C + 0ldc*sizeof(T))
+  NR>1 && vstore(c2, C + 1ldc*sizeof(T))
+  NR>2 && vstore(c3, C + 2ldc*sizeof(T))
+  NR>3 && vstore(c4, C + 3ldc*sizeof(T))
 end
 
